a9730cd699902f27074d7d13537322ae797931bc
Julia Van Cleve <julia@jmv.me>
2015-06-07 19:44:03 -0700

    email gives number of attendees

--- rsvp/app/mailers/guest_mailer.rb
+++ rsvp/app/mailers/guest_mailer.rb
@@ -4,7 +4,7 @@ class GuestMailer < ApplicationMailer
   def guest_email(guest)
     @guest = guest
     attachments.inline['DragonMedium.png'] = File.read('public/images/DragonMedium.png')
-    mail(:to => 'jmvancleve@gmail.com', :subject => 'New RSVP Notification from Julia')
+    mail(:to => 'jmvancleve@gmail.com', :subject => '[LLVM SOCIAL] RSVP Notification ')
   end
 end
 
